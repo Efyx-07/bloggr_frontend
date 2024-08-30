38a145132a22da439f2d2b5c853749e4b4efc5cb
Efyx-07 <fx.savary@orange.fr>
2024-08-31 01:19:01 +0200

    layout

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '../_layouts/MainLayout';
+
+export default function ArticlesPage() {
+  return (
+    <>
+      <MainLayout>
+        <h1>Articles page</h1>
+      </MainLayout>
+    </>
+  );
+}
