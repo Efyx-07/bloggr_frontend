e1f0b3885deacd93e8ea2a71f4ecf2c31c0144b3
Efyx-07 <fx.savary@orange.fr>
2024-08-31 00:08:35 +0200

    adminstore

--- src/stores/adminStore.ts
+++ src/stores/adminStore.ts
@@ -1,15 +1,29 @@
 import { create } from 'zustand';
-import Admin from '@/interfaces/admin.interface';
+import {Admin} from '@/interfaces/admin.interface';
 
 interface State {
   admin: Admin | null;
   token: string | null;
   isLogged: boolean;
+  setToken: (newToken: string | null) => void;
+  saveAdminDataInLocalStorage: () => void;
+  setAdminData: (admin: Admin) => void;
 }
 const useAdminStore = create<State>((set, get) => ({
   admin: null,
   token: null,
   isLogged: false,
+  setToken: (newToken) => {
+    set({ token: newToken, isLogged: !!newToken });
+    localStorage.setItem('token', newToken ?? '');
+  },
+  saveAdminDataInLocalStorage: () => {
+    localStorage.setItem('admin', JSON.stringify(get().admin))
+  },
+  setAdminData: (admin) => {
+    set({admin});
+    get().saveAdminDataInLocalStorage();
+  }
 }));
 
 export default useAdminStore;
