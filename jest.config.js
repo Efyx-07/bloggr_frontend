d918d483c0a7ffbdbfabbe40a4f583f7b33f9c2a
François-Xavier Savary <fx.savary@orange.fr>
2024-08-31 12:23:33 +0200

    setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,13 @@
-// module.exports = {
-//   preset: 'ts-jest',
-//   testEnvironment: 'jsdom',
-//   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-//   moduleNameMapper: {
-//     '^@/(.*)$': '<rootDir>/src/$1',
-//   },
-//   transform: {
-//     '^.+\\.(ts|tsx)?$': 'ts-jest',
-//   },
-//   testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
-//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-// };
-
-// jest.config.js
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
