328f0a7688353e0ca1d281e634f52aca95434afd
François-Xavier Savary <fx.savary@orange.fr>
2024-09-24 10:48:48 +0200

    modify article

--- src/app/modifier-article/[articleId]/page.tsx
+++ src/app/modifier-article/[articleId]/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import '../../../styles/page.scss';
 import MainLayout from '@/app/_layouts/MainLayout';
 import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { Article } from '@/interfaces/article.interface';
 import { fetchArticleById } from '@/services/articles.service';
+import FormContainer from '@/components/FormContainer';
 import UpdateArticleForm from '@/components/Articles-forms/UpdateArticleForm';
 
 export default function UpdateArticlePage() {
@@ -33,8 +35,9 @@ export default function UpdateArticlePage() {
           <div className="content">
             {article ? (
               <>
-                <h1>Modifier article {article?.title}</h1>
-                <UpdateArticleForm selectedArticle={article} />
+                <FormContainer title={`Modifier article: ${article?.title}`}>
+                  <UpdateArticleForm selectedArticle={article} />
+                </FormContainer>
               </>
             ) : (
               <p>No article found</p>
