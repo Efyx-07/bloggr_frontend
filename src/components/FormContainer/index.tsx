bce012aad97f4e133fc91407b9169cd2d7502123
Efyx-07 <fx.savary@orange.fr>
2024-09-28 00:46:17 +0200

    design

--- src/components/FormContainer/index.tsx
+++ src/components/FormContainer/index.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import './FormContainer.scss';
 import HeadTitle from '../Sharables/Others/HeadTitle';
+import { usePathname } from 'next/navigation';
 
 interface FormContainerProps {
   title: string;
@@ -7,8 +10,13 @@ interface FormContainerProps {
 }
 
 export default function FormContainer({ title, children }: FormContainerProps) {
+  const pathName = usePathname();
+  const loginPathname = pathName === '/';
+  const comptePathname = pathName === '/compte';
   return (
-    <div className="form-container">
+    <div
+      className={`form-container ${loginPathname || comptePathname ? 'user-form-container' : ''}`}
+    >
       <HeadTitle title={title} />
       {children}
     </div>
