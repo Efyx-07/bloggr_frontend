be4cf2231cfbd3a029218e40dba17db96dbee2b5
François-Xavier Savary <fx.savary@orange.fr>
2024-09-27 10:36:03 +0200

    hide icon on login page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,24 @@
 
 import './Header.scss';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import SiteName from '../Sharables/Others/SiteName';
 import MobileMenuIcon from './MobileMenuIcon';
 import BurgerMenu from './BurgerMenu';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const pathName: string = usePathname();
 
   return (
     <>
       <header>
         <div className="content">
           <SiteName />
-          <MobileMenuIcon toggleMenu={() => setIsOpen(!isOpen)} />
+          {/* Cache l'icone du menu sur la page de connexion */}
+          {pathName !== '/' && (
+            <MobileMenuIcon toggleMenu={() => setIsOpen(!isOpen)} />
+          )}
         </div>
       </header>
       <BurgerMenu isOpen={isOpen} toggleMenu={() => setIsOpen(!isOpen)} />
