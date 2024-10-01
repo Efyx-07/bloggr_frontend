f9f6133e80b163cfb73e361dc264119335289e69
François-Xavier Savary <fx.savary@orange.fr>
2024-10-01 15:36:31 +0200

    articles form requirement

--- src/components/Sharables/Buttons/PrimaryButton.tsx
+++ src/components/Sharables/Buttons/PrimaryButton.tsx
@@ -1,5 +1,4 @@
 import './FormButton.scss';
-import { useState } from 'react';
 import ButtonLoadingSpinner from '../Spinners/ButtonLoadingSpinner';
 
 interface PrimaryButtonProps {
@@ -17,23 +16,11 @@ export default function PrimaryButton({
   isClicked,
   isLoading,
 }: PrimaryButtonProps) {
-  /*const [isClicked, setIsClicked] = useState<boolean>(false);
-
-  // Modifie l'événement onClick pour gérer à la fois le clic interne et externe
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setIsClicked(true);
-
-    // Appelle la fonction onClick passée en prop si elle existe
-    if (onClick) {
-      onClick(event);
-    }
-  };*/
-
   return (
     <button
       className={`form-button primary ${isClicked ? 'clicked' : ''}`}
       type={type}
-      //onClick={handleClick}
+      onClick={onClick}
     >
       {isLoading ? <ButtonLoadingSpinner /> : <p>{name}</p>}
     </button>
