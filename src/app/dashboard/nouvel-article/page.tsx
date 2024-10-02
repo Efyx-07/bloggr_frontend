61509d48b356d50275577f488a1a5112e8af7eb1
François-Xavier Savary <fx.savary@orange.fr>
2024-10-02 16:31:54 +0200

    FakeComponents

--- src/app/dashboard/nouvel-article/page.tsx
+++ src/app/dashboard/nouvel-article/page.tsx
@@ -3,9 +3,22 @@
 import '@/styles/page.scss';
 import FormContainer from '@/components/FormContainer';
 import CreateArticleForm from '@/components/Articles-forms/CreateArticleForm';
+import FakeArticleForm from '@/components/FakeComponents/FakeArticleForm';
 import LoadingPage from '@/components/LoadingPage';
 import { loadingPageDelay } from '@/config';
+import LoadingSpinner from '@/components/Sharables/Spinners/LoadingSpinner';
 import { useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const DynamicFormContainer = dynamic(
+  () => import('@/components/FormContainer'),
+  {
+    loading: () => <FakeArticleForm />,
+  },
+);
+const DynamicCreateArticleForm = dynamic(
+  () => import('@/components/Articles-forms/CreateArticleForm'),
+);
 
 export default function NewArticlePage() {
   const [isContentVisible, setIsContentVisible] = useState<boolean>(false);
@@ -19,9 +32,9 @@ export default function NewArticlePage() {
       {isContentVisible ? (
         <div className="page">
           <div className="content">
-            <FormContainer title="Nouvel article">
-              <CreateArticleForm />
-            </FormContainer>
+            <DynamicFormContainer title="Nouvel article">
+              <DynamicCreateArticleForm />
+            </DynamicFormContainer>
           </div>
         </div>
       ) : (
---
+++ src/components/FakeComponents/FakeArticleForm.tsx
@@ -0,0 +1,10 @@
+import './FakeArticleForm.scss';
+import LoadingSpinner from '../Sharables/Spinners/LoadingSpinner';
+
+export default function FakeArticleForm() {
+  return (
+    <div className="fake-article-form">
+      <LoadingSpinner />
+    </div>
+  );
+}
