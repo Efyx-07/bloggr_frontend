dddee9fd4b36a3c11431b5f665a320af98ebf6e1
François-Xavier Savary <fx.savary@orange.fr>
2024-10-02 11:35:04 +0200

    header scrolled

--- src/components/Header/MobileMenuIcon.tsx
+++ src/components/Header/MobileMenuIcon.tsx
@@ -3,12 +3,19 @@ import { Icon } from '@iconify/react';
 
 interface MobileMenuIconProps {
   toggleMenu: () => void;
+  scrolled: boolean;
 }
 
-export default function MobileMenuIcon({ toggleMenu }: MobileMenuIconProps) {
+export default function MobileMenuIcon({
+  toggleMenu,
+  scrolled,
+}: MobileMenuIconProps) {
   return (
     <div className="mobile-icon-container" onClick={toggleMenu}>
-      <Icon icon="tabler:menu-3" className="mobile-icon" />
+      <Icon
+        icon="tabler:menu-3"
+        className={` mobile-icon ${scrolled ? 'scrolled' : ''}`}
+      />
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import './Header.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import SiteName from '../Sharables/Others/SiteName';
 import MobileMenuIcon from './MobileMenuIcon';
@@ -9,18 +9,33 @@ import BurgerMenu from './BurgerMenu';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [scrolled, setScrolled] = useState<boolean>(false);
   const pathName: string = usePathname();
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      if (window.scrollY > 0) setScrolled(true);
+      else setScrolled(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      <header>
+      <header className={scrolled ? 'scrolled' : ''}>
         <div
           className={`content ${pathName === '/' ? 'on-login-content' : ''}`}
         >
           <SiteName />
           {/* Cache l'icone du menu sur la page de connexion */}
           {pathName !== '/' && (
-            <MobileMenuIcon toggleMenu={() => setIsOpen(!isOpen)} />
+            <MobileMenuIcon
+              toggleMenu={() => setIsOpen(!isOpen)}
+              scrolled={scrolled}
+            />
           )}
         </div>
       </header>
