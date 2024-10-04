39b6325fd1ee8e644bd6a04dc363bd4d2b764fce
Efyx-07 <fx.savary@orange.fr>
2024-10-04 23:56:39 +0200

    mig tw

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,10 @@
-import './Footer.scss';
 import { siteName } from '@/config';
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer>
-      <p>
+    <footer className="w-full h-28 flex justify-center items-center">
+      <p className="text-sm">
         © {currentYear} {siteName} | Tous droits réservés
       </p>
     </footer>
