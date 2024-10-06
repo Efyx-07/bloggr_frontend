8d6ee3a7d91680b411823f56c4abc38df9fe9616
François-Xavier Savary <fx.savary@orange.fr>
2024-10-06 18:26:27 +0200

    mig tw

--- src/components/FakeComponents/FakeArticleCard.tsx
+++ src/components/FakeComponents/FakeArticleCard.tsx
@@ -4,7 +4,7 @@ import LoadingSpinner from '../Sharables/Spinners/LoadingSpinner';
 export default function FakeArticleCard() {
   return (
     <div className="fake-article-card">
-      <LoadingSpinner />
+      <LoadingSpinner className="large-ring" />
     </div>
   );
 }
--- src/components/FakeComponents/FakeArticleForm.tsx
+++ src/components/FakeComponents/FakeArticleForm.tsx
@@ -4,7 +4,7 @@ import LoadingSpinner from '../Sharables/Spinners/LoadingSpinner';
 export default function FakeArticleForm() {
   return (
     <div className="fake-article-form">
-      <LoadingSpinner />
+      <LoadingSpinner className="large-ring" />
     </div>
   );
 }
