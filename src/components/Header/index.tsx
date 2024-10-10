11b4945067eecf8b10f0e83d80f87075f6f4675e
François-Xavier Savary <fx.savary@orange.fr>
2024-10-10 13:11:25 +0200

    opt navitem header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,9 @@
 import { usePathname } from 'next/navigation';
 import SiteName from './SiteName/SiteName';
 import MobileMenuIcon from './MobileMenuIcon';
+import VerticalSeparator from '../Sharables/Others/VerticalSeparator';
+import Link from 'next/link';
+import { Icon } from '@iconify/react';
 
 export default function Header() {
   const pathName: string = usePathname();
@@ -18,17 +21,37 @@ export default function Header() {
       >
         <div
           className={`
-            w-full max-w-[90rem] 
+            w-full max-w-[90rem] h-8
             px-4 lg:px-8 
             flex items-center
             ${pathName === '/' ? 'justify-center' : 'justify-between'}
           `}
         >
-          <SiteName />
+          <div className="h-full flex items-center gap-8">
+            <SiteName />
+            {/* Ajoute un onglet de navigation sur la page individuelle article */}
+            {pathName.startsWith('/dashboard/article/') && <OptionalNavitem />}
+          </div>
           {/* Cache l'icone du menu sur la page de connexion */}
           {pathName !== '/' && <MobileMenuIcon />}
         </div>
       </header>
     </>
+  );
+}
+
+function OptionalNavitem() {
+  return (
+    <div className="invisible sm:visible h-full flex items-center gap-8">
+      <VerticalSeparator />
+      <Link
+        href="/dashboard/articles"
+        className="text-accent hover:text-black font-semibold
+                    flex items-center gap-2"
+      >
+        <Icon icon="fluent:ios-arrow-24-filled" />
+        <p>retour à mes articles</p>
+      </Link>
+    </div>
   );
 }
---
+++ src/components/Sharables/Others/VerticalSeparator.tsx
@@ -0,0 +1,3 @@
+export default function VerticalSeparator() {
+  return <div className="w-px h-full bg-black10"></div>;
+}
