fd432fbb807c5d8fef3859769e848996f0487fdc
François-Xavier Savary <fx.savary@orange.fr>
2024-10-11 17:29:36 +0200

    art sort dropdown

--- src/components/ArticleSortDropdown/index.tsx
+++ src/components/ArticleSortDropdown/index.tsx
@@ -3,16 +3,18 @@ import './ArticleSortDropdown.css';
 
 export default function ArticleSortDropdown() {
   return (
-    <div className="dropdown-container w-full h-10 relative">
+    <div className="dropdown-container block w-full h-10 relative">
       <select
         className="
-            dropdown
-            relative
-            w-full h-full 
-            px-4
-            text-black
-            border border-black25 rounded
-            cursor-pointer
+          dropdown
+          relative z-10
+          w-full h-full 
+          appearance-none outline-none
+          px-4
+          bg-transparent
+          border border-black25 rounded
+          text-black
+          cursor-pointer
         "
       >
         <option value="">-- Afficher par</option>
@@ -21,7 +23,7 @@ export default function ArticleSortDropdown() {
       </select>
       <Icon
         icon="material-symbols-light:play-arrow"
-        className="dropdown-icon absolute text-black"
+        className="dropdown-icon absolute z-0 text-3xl text-black"
       />
     </div>
   );
