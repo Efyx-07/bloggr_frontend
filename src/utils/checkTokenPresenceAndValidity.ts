a619576fd2cacbdcea634e752a20df8deccaf8ec
François-Xavier Savary <fx.savary@orange.fr>
2024-10-11 16:06:08 +0200

    refacto

--- src/utils/checkTokenPresenceAndValidity.ts
+++ src/utils/checkTokenPresenceAndValidity.ts
@@ -2,7 +2,6 @@ import { isTokenExpired } from './isTokenExpired';
 import { AdminResponse } from '@/interfaces/admin.interface';
 
 // Vérifie la présence et la vaildité d'un token dans le local-storage et renvoie un booléen
-// Prend des paramètres optionnels selon l'utilisation de la fonction
 // ================================================================================================
 export function checkTokenPresenceAndValidity(): boolean {
   // Récupère le token dans le local-storage
