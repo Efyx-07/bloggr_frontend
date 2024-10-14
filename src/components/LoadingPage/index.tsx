52dd2c6fe5811cc78fd886b337df46f43e5ac54f
François-Xavier Savary <fx.savary@orange.fr>
2024-10-14 10:24:44 +0200

    update

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -7,7 +7,7 @@ interface LoadingPageProps {
 export default function LoadingPage({ mention }: LoadingPageProps) {
   return (
     <div
-      className="absolute top-0 left-0 z-5 w-screen h-dvh bg-black text-whiteRelief
+      className="absolute top-0 left-0 z-50 w-screen h-dvh bg-black text-whiteRelief
       flex flex-col justify-center items-center gap-4"
     >
       {mention ? <p>{mention}</p> : <LoadingSpinner className="large-ring" />}
