905a312c78826c23bf2a060e6811e8c11f23d8c4
François-Xavier Savary <fx.savary@orange.fr>
2024-10-15 10:55:55 +0200

    test updatepasswordform

--- src/components/Forms/User-forms/__tests/UpdatePasswordForm.spec.tsx
+++ src/components/Forms/User-forms/__tests/UpdatePasswordForm.spec.tsx
@@ -1,10 +1,15 @@
 import '@testing-library/jest-dom';
 import UpdatePasswordForm from '../UpdatePasswordForm';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 // Mock du service de updatePassword
 jest.mock('@/services/admin.service', () => ({
   updatePassword: jest.fn(),
+}));
+
+// Mock de decodeTokenAndGetAdminId
+jest.mock('@/utils/decodeTokenAndGetAdminId', () => ({
+  decodeTokenAndGetAdminId: jest.fn(),
 }));
 
 // Test de UpdatePasswordForm
@@ -21,5 +26,26 @@ describe('UpdatePasswordForm', () => {
     expect(
       screen.getByRole('button', { name: /Modifier/i }),
     ).toBeInTheDocument();
+  });
+
+  // Test de l'affichage d'erreur en cas d'identifiants invalides ou erreur serveur
+  // ===========================================================================================
+  it('should display an error if credentials are invalid or server error', async () => {
+    render(<UpdatePasswordForm />);
+    // Renseignement de données invalides dans les champs
+    fireEvent.change(screen.getByLabelText(/Votre mot de passe actuel/i), {
+      target: { value: 'invalid current password' },
+    });
+    fireEvent.change(screen.getByLabelText(/Nouveau mot de passe/i), {
+      target: { value: 'invalid new password' },
+    });
+    // Mock la soumission du formulaire avec le bouton
+    fireEvent.submit(screen.getByRole('button', { name: /Modifier/i }));
+    // Affiche le message d'erreur
+    await waitFor(() => {
+      expect(
+        screen.getByText(/Mot de passe invalide ou erreur serveur/i),
+      ).toBeInTheDocument();
+    });
   });
 });
