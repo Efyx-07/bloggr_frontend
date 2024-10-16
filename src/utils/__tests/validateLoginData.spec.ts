df960991851a3e2faca3f1566c8e79a85c5e2630
François-Xavier Savary <fx.savary@orange.fr>
2024-10-16 12:58:00 +0200

    test utils functions

--- src/utils/__tests/validateLoginData.spec.ts
+++ src/utils/__tests/validateLoginData.spec.ts
@@ -1,37 +1,52 @@
 import validateLoginData from '../validateLoginData';
+import { Admin } from '@/interfaces/admin.interface';
 
 // Test de la fonction utilitaire validateLoginData
 // ===========================================================================================
 describe('validateLoginData', () => {
+  const mockGoodEmail: Admin['email'] = 'example@test.com';
+  const mockGoodPassword: Admin['password'] = 'Password1234!';
+  const mockWrongEmail: Admin['email'] = 'invalid-email.com';
+  const mockWrongPassword: Admin['password'] = 'invalid-password';
+
   // Test avec données valides
   it('should return true for valid email and password', async () => {
-    const validEmail = 'example@test.com';
-    const validPassword = 'Password1234!';
-    const isValid = await validateLoginData(validEmail, validPassword);
+    const validEmail: Admin['email'] = mockGoodEmail;
+    const validPassword: Admin['password'] = mockGoodPassword;
+    const isValid: boolean = await validateLoginData(validEmail, validPassword);
     expect(isValid).toBe(true);
   });
 
   // Test avec email invalide
   it('should return false for invalid email', async () => {
-    const invalidEmail = 'invalid-email.com';
-    const validPassword = 'Password1234!';
-    const isValid = await validateLoginData(invalidEmail, validPassword);
+    const invalidEmail: Admin['email'] = mockWrongEmail;
+    const validPassword: Admin['password'] = mockGoodPassword;
+    const isValid: boolean = await validateLoginData(
+      invalidEmail,
+      validPassword,
+    );
     expect(isValid).toBe(false);
   });
 
   // Test avec password invalide
   it('should return true for invalidpassword', async () => {
-    const validEmail = 'example@test.com';
-    const invalidPassword = 'invalid-password';
-    const isValid = await validateLoginData(validEmail, invalidPassword);
+    const validEmail: Admin['email'] = mockGoodEmail;
+    const invalidPassword: Admin['password'] = mockWrongPassword;
+    const isValid: boolean = await validateLoginData(
+      validEmail,
+      invalidPassword,
+    );
     expect(isValid).toBe(false);
   });
 
   // Test avec email et password invalides
   it('should return true for invalid email and invalid password', async () => {
-    const invalidEmail = 'invalid-email.com';
-    const invalidPassword = 'invalid-password';
-    const isValid = await validateLoginData(invalidEmail, invalidPassword);
+    const invalidEmail: Admin['email'] = mockWrongEmail;
+    const invalidPassword: Admin['password'] = mockWrongPassword;
+    const isValid: boolean = await validateLoginData(
+      invalidEmail,
+      invalidPassword,
+    );
     expect(isValid).toBe(false);
   });
 });
