dbb9e6b3eefa8fffd04b755fe702516b9e3dfcc4
François-Xavier Savary <fx.savary@orange.fr>
2024-10-18 13:28:31 +0200

    tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  safelist: [
-    'inter', 
-    'fredoka',
-  ],
   theme: {
     extend: {
       colors: {
